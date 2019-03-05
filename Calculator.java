dff42ffe2119d66f6bff95eddf7bdac6cb949d2c
Kukkar <tanmay.kukkar@capgemini.com>
2019-03-05 10:52:27 +0530

    Added Multiplication function

--- Calculator.java
+++ Calculator.java
@@ -9,4 +9,13 @@ public class Calculator
 	{
 		System.out.println("Addition : "+(number1 + number2));
 	}
+	public void division(int number1,int number2)
+	if(number2==0)
+	{
+		System.out.println(" Number 2 should not be zero");
+	}
+	else
+	{
+		System.out.println("Divion : "+(number1/number2));
+	}
 }	
