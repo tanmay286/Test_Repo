7d5cc5aca43c879ff1ab7c1413690c8c3be25719
Kukkar <tanmay.kukkar@capgemini.com>
2019-03-05 10:27:11 +0530

    Staged and added Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -5,4 +5,8 @@ public class Calculator
 		System.out.println("Hello World !!!");
 		System.out.println("Calculator java Program");
 	}
+	public void addition(int number1,int number2)
+	{
+		System.out.println("Addition : "+(number1 + number2));
+	}
 }	
